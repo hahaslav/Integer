e7da38393522898996b145516009e892154803bf
Yarik32 <yara-2004@ukr.net>
2022-10-11 22:35:03 +0300

    Added method chooser in the main program

--- main.cpp
+++ main.cpp
@@ -14,15 +14,59 @@
 #include "prime/solovey_shtrassen.h"
 #include "prime/ahravala.h"
 
-int main(int argc, char *argv[]) {
-    std::cout << argc << " values:";
-    for (int i = 0; i < argc; i++) {
-        std::cout << "\n" << argv[i];
+int charstring_to_int(char s[]) {
+    int result = 0;
+
+    for (int i = 0; s[i] != 0; i++) {
+        result *= 10;
+        result += s[i] - '0'; // it is converted from the ASCII character code
     }
 
-//    TInteger n(123), s("456");
+    return result;
+}
 
-//    std::cout << n.to_string() << ' ' << s.to_string();
+int main(int argc, char *argv[]) {
+    /*
+     * If executed from a shell,
+     * the program takes these arguments:
+     * - a number of a method
+     * - first integer
+     * - second integer
+     *
+     * Numbers of the methods:
+     * 0 - outputs the given integers
+     * [1; 4] - multiplication methods
+     * 5 - float method
+     * 6 - division method
+     * [7; 10] - prime check methods
+     * 11 - sum
+     * 12 - subtraction
+     * 13 - multiplication "in column"
+     */
+    if (argc == 1) {
+        std::cout << "No arguments were given. Aborting execution.";
+        return 0;
+    }
+    if (argc == 2) {
+        std::cout << "Method " << argv[1] << " was chosen, bun no integers were given. Aborting execution.";
+    }
+
+    TInteger a(argv[2]), b;
+    bool second_integer_exist = argc > 3;
+    if (second_integer_exist) {
+        b = TInteger(argv[3]);
+    }
+
+    switch (charstring_to_int(argv[1])) {
+        case 0:
+            std::cout << a.to_string();
+            if (second_integer_exist) {
+                std::cout << ' ' << b.to_string();
+            }
+            break;
+        default:
+            std::cout << "Not implemented!";
+    }
 
     return 0;
 }

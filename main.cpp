4d65039581813d51e9e54b3f9f910a5f05eecba1
Yarik32 <yara-2004@ukr.net>
2022-10-14 13:48:16 +0300

    Explain main.cpp

--- main.cpp
+++ main.cpp
@@ -17,6 +17,13 @@ std::ostream &operator<<(std::ostream &output, TInteger a) {
 int main() {
     TInteger a(123), b("456");
 
+    // Should output:
+    // 12345
+    // 54321
+
+    // It means, that Karatsuba was the first method
+    // and Modular the second
+
     Multiplication* method = new Karatsuba;
     std::cout << method->multiply(a, b) << "\n";
 

3d940432813e4d7df3d471d4e78beff78a78fdd9
Yarik32 <yara-2004@ukr.net>
2022-10-19 09:32:59 +0300

    Add steps description for Tom-Cook

--- multiplication/tom_cook.cpp
+++ multiplication/tom_cook.cpp
@@ -6,5 +6,33 @@
 // make Tom-3
 
 TInteger TomCook::multiply(TInteger a, TInteger b) {
+    if (std::abs((int)a) < BASE * BASE || std::abs((int)b) < BASE * BASE) {
+        return a * b;
+    }
+
+    bool to_invert = negative_after_multiplication(a, b);
+
+    // splitting
+
+
+    // evaluation
+
+
+    // pointwise multiplication
+
+
+    // interpolation
+    /*
+     *   /  6 0  0  0  0  \ /  r(0) \
+     * 1 |  3 2 -6  1 -12 | |  r(1) |
+     * - | -6 3  3  0  -6 | | r(-1) |
+     * 6 | -3 1  3 -1  12 | | r(-2) |
+     *   \  0 0  0  0  6  / \  r(∞) /
+     */
+
+
+    // recomposition
+
+
     return TInteger(59932);
 }
